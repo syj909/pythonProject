a60d313554b9a48205ee6d38805caf28cbe2e2ef
syj909 <syj909@github.com>
2022-10-11 18:18:31 +0900

    변수, 연산자, 예제

---
+++ 04examples.py
@@ -0,0 +1,51 @@
+# 1
+print("*   *      **      ****      ****     *     *    /////")
+print("*   *     *  *     *    *    *    *    *   *    | O O | ")
+print("*****    *    *    ****      ****       * *    (|  ^  |) ")
+print("*   *   ********   *    *    *    *      *      | [_] | ")
+print("*   *  *        *  *     *   *     *     *       ----- ")
+print("\n\n")
+print("            +---+")
+print("            |   |")
+print("        +---+---+")
+print("        |   |   |")
+print("    +---+---+---+       /\\_/\\       -----")
+print("    |   |   |   |      ( ' ' )    / Hello \\")
+print("+---+---+---+---+      (  -  )   <  Junior |")
+print("|   |   |   |   |       | | |     \\ Coder!/")
+print("+---+---+---+---+      (__|__)      -----")
+# 3
+# 1stPlayer
+# myprogram.java
+
+# 4
+x, y, z = 1, 2, 3
+s0, v0, t, g = 4, 5, 6, 9.8
+print(3 * x, 3 * x + y)
+print(s0 + v0*t + (1/2) * g * t**2)
+#6
+x, y, m, n = 2.5, 1.5, 18, 4
+
+print(x + n * y(x+n) * y)
+print(m / n + m % n)
+print(5 * x - n / 5)
+print(1 - (1 - (1 - (1 - (1 - n)))))
+
+#7
+print(3 + 4.5 * 2 +27 /8)
+# 논리연산시 경우에 따라 단축식 평가가 적용되기도 함.
+print(True or False and 3 < 4 or not (5==7))
+
+#print (not True > 'A')
+print (not True > bool('A')) # print(not True > True)
+
+#print(7 % 4 + 3 - 2 / 6 * 'Z')
+print(7 % 4 + 3 - 2 / 6 * bool('Z'))
+
+#print('D' + 1 + 'M' % 2 / 3)
+print(bool('D') + 1 + bool('M') % 2 / 3)  #(1 + 1 + 1 % 2 / 3)
+
+print(5.0 / 3. + 3 / 3)
+print(53 % 21 < 45 / 18)
+
+print((4 < 6) or True and False and (2 > 3)) # ((4 < 6) or False and False)
